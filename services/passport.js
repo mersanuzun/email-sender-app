527c4294d0ec5e75d03429af2a746553e7c27133
Mehmet Ersan Uzun <mersanuzun@gmail.com>
2018-06-09 16:24:03 +0300

    Added proxy flag

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
         {
             clientID: keys.google.clientId,
             clientSecret: keys.google.secretId,
-            callbackURL: "/auth/google/callback"
+            callbackURL: "/auth/google/callback",
+            proxy: true
         }, 
         (accessToken, refreshToken, profile, done) => {
             User.findOne({googleId: profile.id})
