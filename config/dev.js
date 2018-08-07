1c7f4871f2434a3cff6dd42780a11207db244fec
Mehmet Ersan UZUN <mersanuzun@gmail.com>
2018-08-07 16:50:28 +0300

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,20 +1,20 @@
 module.exports = {
     google: {
-        clientId: "132966515161-758sefsp634q5mt4354bnvrvm3mhhhh9.apps.googleusercontent.com",
-        secretId: "7uyV4D-0zqD8cA45iFMxs9tN"
+        clientId: "****",
+        secretId: "****"
     },
     mongoDb: {
-        dbURI: "mongodb://mersanuzun:qwer1234@ds147180.mlab.com:47180/emaily_dev_db"
+        dbURI: "****"
     },
     cookieSession: {
-        keys: ["qwdfskglsmflad"]
+        keys: ["****"]
     },
     stripe: {
-        publisableKey: "pk_test_82A7fyR7n17QK69WHpWEmHxp",
-        secretKey: "sk_test_X0RUGKNbcVOaaw0OKlj3LeDb"
+        publisableKey: "*****",
+        secretKey: "******"
     },
     sendGrid: {
-        apiKey: "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"
+        apiKey: "******"
     },
     server: {
         domain: "http://localhost:3000/surveys"
